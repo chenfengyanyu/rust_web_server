8633c6ecb70c176f2cb538b1f9a02bd19160a924
Jartto <315764765@qq.com>
2023-01-05 08:04:23 +0800

    feat: add TCP  stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
 use std::net::TcpListener;
 fn main() {
     // 监听 TCP 连接
@@ -8,6 +10,112 @@ fn main() {
         // connection 则代表了客户端连接服务器、服务器生成响应，以及服务器关闭连接的全部请求与响应过程
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        // println!("Connection established!");
+        handle_connection(stream);
+
     }
 }
+
+// 从 TCP 流内读取数据
+fn handle_connection(mut stream: TcpStream) {
+    // 在栈上声明一个用于存放数据的 buffer，512 字节的缓冲区
+    let mut buffer  = [0;512];
+    // 使用缓冲区调用 stream.read，它会从 TcpStream 中读取数据并将其存储至缓冲区中
+    stream.read(&mut buffer).unwrap();
+
+    // String::from_utf8_lossy 可以接收一个 &[u8] 并产生对应的 String
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
+
+// 访问 http://127.0.0.1:7878/
+
+// Request: GET / HTTP/1.1
+// Host: 127.0.0.1:7878
+// Connection: keep-alive
+// sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
+// sec-ch-ua-mobile: ?0
+// sec-ch-ua-platform: "macOS"
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+// Sec-Fetch-Site:
+// Request: GET / HTTP/1.1
+// Host: 127.0.0.1:7878
+// Connection: keep-alive
+// sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
+// sec-ch-ua-mobile: ?0
+// sec-ch-ua-platform: "macOS"
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+// Sec-Fetch-Site:
+// Request: GET / HTTP/1.1
+// Host: 127.0.0.1:7878
+// Connection: keep-alive
+// sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
+// sec-ch-ua-mobile: ?0
+// sec-ch-ua-platform: "macOS"
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+// Sec-Fetch-Site:
+// Request: GET / HTTP/1.1
+// Host: 127.0.0.1:7878
+// Connection: keep-alive
+// Cache-Control: max-age=0
+// sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
+// sec-ch-ua-mobile: ?0
+// sec-ch-ua-platform: "macOS"
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v
+// Request: GET / HTTP/1.1
+// Host: 127.0.0.1:7878
+// Connection: keep-alive
+// Cache-Control: max-age=0
+// sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
+// sec-ch-ua-mobile: ?0
+// sec-ch-ua-platform: "macOS"
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v
+// Request: GET / HTTP/1.1
+// Host: 127.0.0.1:7878
+// Connection: keep-alive
+// Cache-Control: max-age=0
+// sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
+// sec-ch-ua-mobile: ?0
+// sec-ch-ua-platform: "macOS"
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v
+// Request: GET / HTTP/1.1
+// Host: 127.0.0.1:7878
+// Connection: keep-alive
+// Cache-Control: max-age=0
+// sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
+// sec-ch-ua-mobile: ?0
+// sec-ch-ua-platform: "macOS"
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v
+// Request: GET / HTTP/1.1
+// Host: 127.0.0.1:7878
+// Connection: keep-alive
+// Cache-Control: max-age=0
+// sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
+// sec-ch-ua-mobile: ?0
+// sec-ch-ua-platform: "macOS"
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v
+// Request: GET / HTTP/1.1
+// Host: 127.0.0.1:7878
+// Connection: keep-alive
+// Cache-Control: max-age=0
+// sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
+// sec-ch-ua-mobile: ?0
+// sec-ch-ua-platform: "macOS"
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v
